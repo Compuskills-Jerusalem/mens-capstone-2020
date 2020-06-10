fd81da4dc256a4e56b283a1f2a3cd3b822d938ac
ykosbie <ykosbie@gmail.com>
2020-06-10 21:39:39 +0300

    Add MVC scaffolding for project

---
+++ src/Mens2020.Mvc/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Mens2020.Mvc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ src/Mens2020.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mens2020.Mvc.MvcApplication" Language="C#" %>
