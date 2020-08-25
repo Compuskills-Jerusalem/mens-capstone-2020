ed8b9eb2d577ff2f8a4cb329a071faa31b1ed038
Nathan Simon <nossondsimon@gmail.com>
2020-08-25 22:02:03 +0300

    Goal creation MVC model/database support #57
    Added model binding and updating functionality for goal creation along with basic scaffolding for the user goal creation. Need to merge goal controller and its views with the new UserEvents Controller

--- src/Mens2020.Mvc/Controllers/_GoalController.cs
+++ src/Mens2020.Mvc/Controllers/_GoalController.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Mens2020.Mvc.Models;
+using Mens2020.DataSource.Models;
+using Mens2020.DataSource.DataSource;
+using System.Collections.ObjectModel;
+using System.Runtime.Remoting.Contexts;
+using System.Data.Entity;
 
 namespace Mens2020.Mvc.Controllers
 {
+    [Authorize]
     public class _GoalController : Controller
     {
         // GET: _Goal
@@ -22,52 +34,147 @@ namespace Mens2020.Mvc.Controllers
 
         // GET: _Goal/Create
         public ActionResult Create()
+
+
         {
+            
             return View();
+
+          
         }
 
         // POST: _Goal/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        // public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate")] Student student)
+     
+        public ActionResult Create( CreateGoalModel model )
         {
-            try
-            {
-                // TODO: Add insert logic here
+            // public ActionResult Create([Bind( Include = FormCollection collection)
+            
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+
+
+               
+                    //if (ModelState.IsValid)
+                    //{
+                        using (var db = new Capstone2020Context(nameof(Capstone2020Context)))
+                        {
+
+
+                        model.UserEventId = Guid.NewGuid().ToString();
+                        model.RevisionID =  0;
+                //model.EventTitle = "This is my first goal";
+                //model.EventText = "This is the text of my first goal";
+
+
+                //if (User.Identity.IsAuthenticated)
+                //{
+
+                //}
+                            var @event = new UserEvent
+                            {
+                                UserEventId = model.UserEventId,
+                                UserId = User.Identity.GetUserId(),
+                                ColorID = 1,
+                                CompletedDate = model.CompletedDate,
+                                CreationDate = DateTime.Now,
+                                EventTitle = model.EventTitle,
+                                EventText = model.EventText,
+                                RecurID = 0,
+                                EventTypeId = 1
+
+                            };
+               string CurrentUserId = User.Identity.GetUserId();
+                db.Set<UserEvent>().Add(@event);
+
+                db.SaveChanges();
+                
+                      
+                        }
+                   
+
+                    
+                    //Capstone2020Context db = new Capstone2020Context();
+                    
+                        
+                        // TODO: Add insert logic here
+                        //var userEvent = new UserEvent();
+
+                        
+                        
+                        //string GoalTitle = Request.Form["Goal"];
+
+                        //userEvent.EventTitle = GoalTitle;
+
+                        //userEvent.UserEventId = new Guid();
+
+                        //userEvent.CreationDate = DateTime.Now;
+
+                        //userEvent.EventTypeId = 1;
+
+                        
+
+
+
+
+                        //TODO: onbuttionlickforsavecheck for working database intigration
+
+
+
+
+
+                        return RedirectToAction("Index");
+                    //}
+                    //else
+                    //{
+                    //    return View(model);
+                    //}
+              
+            
         }
-
         // GET: _Goal/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+          
+                return View();
+
         }
 
         // POST: _Goal/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, CreateGoalModel model)
         {
-            try
+            if(ModelState.IsValid)
             {
                 // TODO: Add update logic here
+                using (var db = new Capstone2020Context(nameof(Capstone2020Context)))
+                {
+                    model.RevisionID = model.RevisionID + 1;
+
+
+
+
+                    db.SaveChanges();
+                  
+                }
+
+
 
                 return RedirectToAction("Index");
-            }
-            catch
+                }
+            
+            else
             {
-                return View();
+                return View(model);
             }
         }
 
         // GET: _Goal/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+          
+
+                return View();
         }
 
         // POST: _Goal/Delete/5
@@ -75,7 +182,15 @@ namespace Mens2020.Mvc.Controllers
         public ActionResult Delete(int id, FormCollection collection)
         {
             try
+
             {
+
+                using (var db = new Capstone2020Context(nameof(Capstone2020Context)))
+                {
+                   // db.UserEvents.Remove(UserEvent id);
+
+
+                }
                 // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
