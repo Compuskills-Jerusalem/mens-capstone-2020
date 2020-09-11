ad43273a4d6dbf7074e6625d89387a2457d6e5dd
DanielCrossman <dan.crossman@yahoo.com>
2020-09-11 15:41:44 +0300

    Added: WebApi Packages, Config file, Controller, and Http to Global.asax.cs #66

---
+++ src/Mens2020.Mvc/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Mens2020.Mvc.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "MentorEmail",
+                routeTemplate: "api/MailToApi",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Setup json serialization to serialize classes to camel (std. Json format)
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver =
+                new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
