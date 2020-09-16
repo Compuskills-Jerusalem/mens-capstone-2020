32f751136ecdace63772528bf6e21ac09a44b11b
Nathan Simon <nossondsimon@gmail.com>
2020-09-16 18:03:11 +0300

    adding ConvertToDay method for LINQ query #46

--- src/Mens2020.DataSource/Models/Day.cs
+++ src/Mens2020.DataSource/Models/Day.cs
@@ -6,31 +6,65 @@ using System.Threading.Tasks;
 
 namespace Mens2020.DataSource.Models
 {
-   
-        [Flags]
-        public enum Day
+
+    [Flags]
+    public enum Day
+    {
+        None = 0,
+        Monday = 2,
+        Tuesday = 4,
+        Wednesday = 8,
+        Thursday = 16,
+        Friday = 32,
+        Saturday = 64,
+        Sunday = 128,
+        Weekdays = Day.Monday | Day.Tuesday | Day.Wednesday | Day.Thursday | Day.Friday,
+        Weekends = Day.Saturday | Day.Sunday,
+        AllDaysOfWeek = Weekdays | Weekends
+
+
+
+
+
+
+    }
+
+    public static class DayExtentions
+    {
+
+        public static Day ConvertToDay(this DayOfWeek source)
         {
-            None = 0,
-            Monday = 2,
-            Tuesday = 4,
-            Wednesday = 8,
-            Thursday = 16,
-            Friday = 32,
-            Saturday = 64,
-            Sunday = 128,
-            Weekdays = Day.Monday | Day.Tuesday | Day.Wednesday | Day.Thursday | Day.Friday,
-            Weekends = Day.Saturday | Day.Sunday,
-            AllDaysOfWeek = Weekdays | Weekends
+            switch (source)
+            {
+                case DayOfWeek.Sunday:
+                    return Day.Sunday;
 
+                case DayOfWeek.Monday:
+                    return Day.Monday;
 
+                case DayOfWeek.Tuesday:
+                    return Day.Tuesday;
 
+                case DayOfWeek.Wednesday:
+                    return Day.Wednesday;
 
+                case DayOfWeek.Thursday:
+                    return Day.Thursday;
 
+                case DayOfWeek.Friday:
+                    return Day.Friday;
 
+                case DayOfWeek.Saturday:
+                    return Day.Saturday;
+
+                default:
+                    return Day.None;
+            }
         }
-        //Day UserEventRecurrence;
-        //UserEventRecurrence = Day.Friday | Day.Monday | Day.Tuesday;
-        //    int bitvaluse = (int)UserEventRecurrence;
-        //Day readvaule = (Day)bitvaluse;
-   
+       
+
+
+    }
+    
+
 }
