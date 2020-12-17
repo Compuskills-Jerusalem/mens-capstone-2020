6c1cc6825b8ecdb3b44fd27ba95d630786c9c699
Naftoli Ost <naftoliyost@gmail.com>
2020-12-17 23:05:53 +0200

    Adding infrastructure to send emails

---
+++ src/Mens2020.Mvc/Email.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using System.Web.Configuration;
+
+namespace Mens2020.Mvc
+{
+    public class Email
+    {
+        public static void Send(string to, string subject, string body)
+        {
+            MailAddress fromMailAddress = new MailAddress(WebConfigurationManager.AppSettings["senderEmail"], WebConfigurationManager.AppSettings["senderDisplayName"]);
+            MailAddress toMailAddress = new MailAddress(to);
+
+            MailMessage mail = new MailMessage(fromMailAddress, toMailAddress);
+
+            mail.ReplyToList.Add(WebConfigurationManager.AppSettings["senderReplyToEmail"]);
+            mail.Subject = subject;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient();
+
+            smtpClient.Send(mail);
+        }
+    }
+}
